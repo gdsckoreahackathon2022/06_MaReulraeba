aca004b3dadeb5a7ab9ce181f2f671fa15259e2d
j1mmyson <bwson98@gmail.com>
2022-02-04 23:25:15 +0900

    init flutter project

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+import 'main_page.dart';
+// import 'game_page.dart';
+
+// import 'package:flutter_svg/flutter_svg.dart';
+
+void main() {
+  runApp(const MaterialApp(
+    title: "MainPage",
+    home: MyApp(),
+  ));
+  // runApp(const MyApp());
+}
