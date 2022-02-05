f32f3bfec4ca98d6ca05befeaaf7f87165563781
Minyoung52 <90830500+Minyoung52@users.noreply.github.com>
2022-02-05 10:17:51 +0900

    save_files

---
+++ lib/save_files.dart
@@ -0,0 +1,202 @@
+import 'package:flutter/material.dart';
+
+class SaveFilesPage extends StatelessWidget {
+  const SaveFilesPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: PreferredSize(
+        preferredSize: const Size.fromHeight(50.0),
+        child: AppBar(
+          title: const Text('이어하기',
+              style: TextStyle(
+                color: Colors.black,
+              )),
+          backgroundColor: Colors.white,
+          centerTitle: true,
+          elevation: 5.0,
+          leading: IconButton(
+            icon: const Icon(Icons.arrow_back_ios_new),
+            color: Colors.black,
+            onPressed: () {
+              // onPressed
+            },
+          ),
+        ),
+      ),
+      body: Center(
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+              SizedBox(
+                height: 50,
+              ),
+              Row(
+                children: [
+                  const SizedBox(
+                    width: 17,
+                  ),
+                  Text(
+                    "대학교 안에서",
+                    style: const TextStyle(
+                        color: Colors.grey, fontWeight: FontWeight.bold),
+                  ),
+                ],
+              ),
+              const SizedBox(height: 10),
+              InkWell(
+                onTap: () {
+                  print("대학교 안에서");
+                },
+                child: Container(
+                  margin: const EdgeInsets.only(bottom: 40),
+                  height: 100,
+                  width: 400,
+                  // alignment: Alignment.center,
+                  decoration: BoxDecoration(
+                    border: Border.all(
+                      color: Colors.black,
+                      width: 2,
+                    ),
+                    color: Colors.grey[200],
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Row(
+                        // mainAxisAlignment: MainAxisAlignment.start,
+                        children: [
+                          const SizedBox(
+                            width: 20,
+                          ),
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.center,
+                            children: const <Widget>[
+                              SizedBox(
+                                height: 15,
+                              ),
+                              Text(
+                                "01",
+                                style: TextStyle(
+                                  fontSize: 40,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                              Text("STAGE",
+                                  style: TextStyle(
+                                    fontWeight: FontWeight.bold,
+                                  )),
+                            ],
+                          ),
+                          const SizedBox(
+                            width: 15,
+                          ),
+                          const SizedBox(
+                            height: 75,
+                            child: VerticalDivider(
+                              thickness: 1,
+                              color: Colors.grey,
+                            ),
+                          ),
+                        ],
+                      ),
+                      Row(
+                        children: [
+                          Column(
+                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: const <Widget>[
+                              Text(
+                                "●   2022-02-04 12:00:04",
+                                style: TextStyle(fontSize: 14),
+                              ),
+                              // SizedBox(
+                              //   height: 10,
+                              // ),
+                              Text(
+                                "●   영희의 방 ----",
+                                style: TextStyle(fontSize: 14),
+                              ),
+                            ],
+                          ),
+                          const SizedBox(
+                            width: 40,
+                          )
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              SavedBox(title: "연락 속에서"),
+              SavedBox(title: "만남 안에서"),
+              SavedBox(title: "SNS에서"),
+              SavedBox(title: "회사 안에서"),
+              SavedBox(title: "여행 속에서"),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class SavedBox extends StatelessWidget {
+  final String title;
+  const SavedBox({Key? key, required this.title}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Row(
+          children: [
+            const SizedBox(
+              width: 17,
+            ),
+            Text(
+              title,
+              style: const TextStyle(
+                  color: Colors.grey, fontWeight: FontWeight.bold),
+            ),
+          ],
+        ),
+        const SizedBox(height: 10),
+        InkWell(
+          onTap: () {
+            print(title);
+          },
+          child: Container(
+            margin: const EdgeInsets.only(bottom: 40),
+            height: 100,
+            width: 400,
+            // alignment: Alignment.center,
+            decoration: BoxDecoration(
+              border: Border.all(
+                color: Colors.black,
+                width: 2,
+              ),
+              color: Colors.grey[200],
+              borderRadius: BorderRadius.circular(10),
+            ),
+            child: Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Icon(
+                    Icons.lock_outline,
+                    color: Color(0xff949494),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
